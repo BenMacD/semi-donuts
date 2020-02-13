8574e9357bad94a725ce28d4774f91cc5323fb96
BenMacD <benmdonagh@gmail.com>
2020-02-13 13:59:21 +0000

    Updates

---
+++ precache-manifest.a1cac58816a157f8b6fd1b3cf3b91814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403bd38af231c5820e3201dce7f2f58b",
+    "url": "/semi-donuts/index.html"
+  },
+  {
+    "revision": "be18966798476216cb24",
+    "url": "/semi-donuts/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "19a36818b95722c57926",
+    "url": "/semi-donuts/static/js/2.52b5d088.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/semi-donuts/static/js/2.52b5d088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be18966798476216cb24",
+    "url": "/semi-donuts/static/js/main.0dacc1d9.chunk.js"
+  },
+  {
+    "revision": "2245358f8180610404c4",
+    "url": "/semi-donuts/static/js/runtime-main.5a7440e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/semi-donuts/static/media/logo.5d5d9eef.svg"
+  }
+]);
